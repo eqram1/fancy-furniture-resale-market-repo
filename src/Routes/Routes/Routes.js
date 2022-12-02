1a2505ba48cd131d3052600cd335500357cdfbf5
eqram1 <abmeqramhossain@gmail.com>
2022-12-02 07:59:49 +0600

    added order section

--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -1,6 +1,7 @@
 import { createBrowserRouter, Router } from "react-router-dom"
 import Main from "../../Layout/Main"
 import Home from "../../Pages/Home/Home/Home"
+import Order from "../../Pages/Home/Order/Order";
 import Login from "../../Pages/Login/Login";
 import Logout from "../../Pages/Logout/Logout";
 
@@ -12,6 +13,10 @@ const router = createBrowserRouter([
             {
                 path: '/',
                 element: <Home></Home>
+            },
+            {
+                path: '/order',
+                element: <Order></Order>
             },
             {
                 path: '/login',
